9596a7eeab0ef5866202e6e39c944a6ca2c9f0dd
Aleksandra Temereva <aleksandra.temereva@epita.fr>
2026-03-16 11:20:52 +0100

    styles adjustments

--- frontend/src/scene/ResponsiveCamera.jsx
+++ frontend/src/scene/ResponsiveCamera.jsx
@@ -5,11 +5,26 @@ export default function ResponsiveCamera() {
   const { camera, size } = useThree();
 
   useEffect(() => {
-    if (size.width < 600) {
-      camera.position.set(0, 0, 8);
-    } else {
-      camera.position.set(0, 0, 6);
+    const aspectRatio = size.width / size.height;
+    let distance = 6;
+
+    if (size.width < 420) {
+      distance = 9.1;
+    } else if (size.width < 640) {
+      distance = 8.2;
+    } else if (size.width < 900) {
+      distance = 7.5;
     }
+
+    if (size.height < 760) {
+      distance += 0.45;
+    }
+
+    if (aspectRatio < 0.7) {
+      distance += 0.35;
+    }
+
+    camera.position.set(0, 0, distance);
   }, [size, camera]);
 
   return null;
